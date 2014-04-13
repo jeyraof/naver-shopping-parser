8f05bd2d35cd75ff55c47626fca13e2af0755d7d
Jaeyoung Lee <jaeyoung@monodiary.net>
2014-04-13 09:02:09 +0900

    write down control exception

--- shopping_parser/parser/exceptions.py
+++ shopping_parser/parser/exceptions.py
@@ -18,3 +18,39 @@ class NoUrlGivenError(Error):
 
     def __str__(self):
         return str(self.msg)
+
+
+class NoPreviousSearchError(Error):
+    """
+    Exception for not following search.
+    """
+
+    def __init__(self, msg=u'Not following search. First of all, search using search_by().'):
+        self.msg = msg
+
+    def __str__(self):
+        return str(self.msg)
+
+
+class NoMorePreviousPageError(Error):
+    """
+    Exception for no more previous page.
+    """
+
+    def __init__(self, msg=u'No more previous page.'):
+        self.msg = msg
+
+    def __str__(self):
+        return str(self.msg)
+
+
+class NoMoreNextPageError(Error):
+    """
+    Exception for no more next page.
+    """
+
+    def __init__(self, msg=u'No more next page.'):
+        self.msg = msg
+
+    def __str__(self):
+        return str(self.msg)
